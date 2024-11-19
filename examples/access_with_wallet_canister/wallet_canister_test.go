b7307fc3a8f3b54886a6d59c1e208aa5acdd573b
readygo67 <readygo67@yeah.net>
2024-11-19 16:15:19 +0800

    add Test_VerifyandSign_Mainnet_redeem_0df910a0a0aba382efdda61a52043b2c4f2ea6b62cdefbb17f4e8ee13845c7d5 in

--- examples/access_with_wallet_canister/wallet_canister_test.go
+++ examples/access_with_wallet_canister/wallet_canister_test.go
@@ -238,3 +238,60 @@ func Test_VerifyandSign_Mainnet_With_Cycles_Debug(t *testing.T) {
 	assert.NoError(t, err)
 	fmt.Printf("result:%v\n", result.Ok.Return)
 }
+
+func Test_VerifyandSign_Mainnet_redeem_0df910a0a0aba382efdda61a52043b2c4f2ea6b62cdefbb17f4e8ee13845c7d5(t *testing.T) {
+	id, err := identity.NewSecp256k1IdentityFromPEMWithoutParameters(pem)
+	assert.NoError(t, err)
+
+	host, err := url.Parse("https://icp0.io/")
+	assert.NoError(t, err)
+
+	cfg := agent.Config{
+		Identity:                       id,
+		ClientConfig:                   &agent.ClientConfig{Host: host},
+		FetchRootKey:                   true,
+		PollTimeout:                    30 * time.Second,
+		DisableSignedQueryVerification: false, //MUST BE TRUE TO ACCESS LOCAL REPLICA
+	}
+	a, err := wallet.NewAgent(principal.MustDecode("4ichq-aqaaa-aaaak-qcosa-cai"), cfg) //这里的canister id是 dfx identity get-wallet --network ic 结果
+	assert.NoError(t, err)
+
+	balance, err := a.WalletBalance()
+	assert.NoError(t, err)
+	fmt.Printf("balance:%v\n", balance)
+
+	canisterId := principal.MustDecode("5jpjw-iaaaa-aaaak-qiuqa-cai")
+
+	var s1 string
+	err = a.Query(canisterId, "witness_builder_canister", []any{}, []any{&s1})
+	assert.NoError(t, err)
+	assert.Equal(t, "47er5-5qaaa-aaaak-qiuva-cai", s1)
+
+	var s2 string
+	err = a.Query(canisterId, "plonk_verifier_canister", []any{}, []any{&s2})
+	assert.NoError(t, err)
+	assert.Equal(t, "ysvvh-oqaaa-aaaak-qiupa-cai", s2)
+
+	//step4: build witness
+	txIDHex := "0F9D4C3FC125D6F3BB59B5323932283943F5D7C0F812DAC83A43F3914330184C"
+	signHashesHex := []string{"37A53D761E4E9A9C0D7A27DB31720E15E0FDE21C3527EBE78EBA3E37A032F782", "D63F90B7C4151E4CF5763E5ACA488E0002331A7BFE8E5D3CE6C22CD946CAA04B", "259B02FED3B6874E8503769C4EE28153439994B8FA5845347046EEC88E7B11AD", "DF0DF8900DB9839EC023F5DE01F386F24F910875D0DEDC3E656929BABAE8A3A4"}
+	proofHex := "20d5a9dd6fdff65a217259d6be1f44adb89e989a544eccb7ab7cffd8da29cb471d933e0c3d68694ad8ef3f75cfeaebf257e0739132487a393b47ab73cd968657281083a595c886a1f08c647587b529f6648d0685816d7becf743c77194ca8c0f2e068e395a51f4dd81408aef87256cd3a49b7dc9b154e9bfeeb5eae57abaff592168b757ca5ad1dfacc881a0f274b82d99450775269b16b0e8ca68740ca7eda2087544f3a0eb8dad09b789e6bf7015241ab1b22d07f8935e7c30a4b61da0ab8c044214fe0f4d327dc54bf7aa42c8dfcbcc7d6e6fdb60482ebb40d31592d213d60119a221a4ddeb8ec8046a0c97ab4a5dc9fd9c62a6d5fc6e7e0248f107c54cec26fd12aeb4a6fe5e104835eb840b5cbecfec71badaf34fb6bbfc55c1038c6e9f084d2a663cfc7b00222f704975436378f222bc9e7408c5c43b0a941f681839a82b6deaaf74eb2b3296132cbe3d933effac47ef7f8b8c999ce88bb355ec5b98e31bac52e9dc29c3c9b2b6405664cc504d78bcec66dd767328cf6c1c384991ffd50e4224fa492dc1afa95fd41a728b078c03448c39ef5402aa44c099d6103a00f908b2823c0905535e35caa7c121171f35984a988e435ece2b6e8aed27d50601b904d862bedd7e2bebfe88fcae9b8e5732dee24e6b6f104d55b597213f6b9ee2732d71932cb49ddff2537cf26624f39b6d65d22b96a5a341a1ffdd2e66d25ad4fc25f3bb290e5288a1e944ea0e31541db7fbb63394ab149bb35d4263e8d68fdc4e0f13b9ac4e6db0725196f34ae82043a92cf65b55b65661635cd31d80793dd3022d23e9deabd833085119f97fecf7011126b296952581f9c74379662b1136434300f3b578df057e986af4185bec6167f79a79d1b5e6da3c3aaf068d8610de42b8208f2b0b9c97770d230a7be74dc7b44256b41f6e3182f5d8f46a7043ab44994f1852315f915c6bd2c37013e84a76c43b85df3c662af4ef00698374059d7d13352251a4fc2f78bd5f219ffd54d3d4978264a8038745950fef9e854ab8e7d2c0331021dd1b4e66c428e00c83cb01571f2693b66bf1e6bb09b32d1caf31b6b925800123809da63ff127f53a642ddd90b4730916c6ed2f6b1277557de68619c8e1ff0e4b4aa08ab94044b916f014c0c38f557bfbf6488e489ebceadaa6b5785488a4017d739805c3f921a6957290e194e525aec78ba55793a40163dadc8db96bb7641f0090749cc5a89d6d28772ebd78d6344064a29dea7a72fff322c572f98250de0ad5ff15baed2d7f72147e134f408c901ab1c5dc78ec2a79afe9af51f8e576ec"
+
+	input4, err := idl.Marshal([]any{txIDHex, signHashesHex, proofHex})
+	assert.NoError(t, err)
+
+	arg4 := struct {
+		Canister   principal.Principal `ic:"canister" json:"canister"`
+		MethodName string              `ic:"method_name" json:"method_name"`
+		Args       []byte              `ic:"args" json:"args"`
+		Cycles     uint64              `ic:"cycles" json:"cycles"`
+	}{
+		Canister:   canisterId,
+		MethodName: "verify_and_sign",
+		Args:       input4,
+		Cycles:     SIGN_AND_VERIFY_FEE,
+	}
+	result, err := a.WalletCall(arg4)
+	assert.NoError(t, err)
+	fmt.Printf("result:%v\n", result.Ok.Return)
+}
